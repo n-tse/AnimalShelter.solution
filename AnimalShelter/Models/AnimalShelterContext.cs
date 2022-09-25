b76eaa0310d52e1c54845533edc16a28bcce69bd
Nick Tse <nicholashtse@gmail.com>
2022-09-24 19:58:32 -0700

    add more json seeding

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -18,7 +18,13 @@ namespace AnimalShelter.Models
               new Animal { AnimalId = 3, Name = "Matilda", Species = "Rabbit", Age = 2 },
               new Animal { AnimalId = 4, Name = "Pip", Species = "Bird", Age = 4 },
               new Animal { AnimalId = 5, Name = "Bartholomew", Species = "Guinea Pig", Age = 22 }
-          );
+              new Animal { AnimalId = 6, Name = "Fido", Species = "Cat", Age = 12 },
+              new Animal { AnimalId = 7, Name = "Abracadabraalakazam", Species = "Cat", Age = 8 }
+              new Animal { AnimalId = 8, Name = "Bigfoot", Species = "Gecko", Age = 4 },
+              new Animal { AnimalId = 9, Name = "Casper", Species = "Bird", Age = 9 },
+              new Animal { AnimalId = 10, Name = "Leon", Species = "Rabbit", Age = 16 },
+              new Animal { AnimalId = 11, Name = "Coco", Species = "Dog", Age = 13 },
+              new Animal { AnimalId = 12, Name = "Scout", Species = "Cat", Age = 6 }
       }
 
       public DbSet<Animal> Animals { get; set; }
