8680526c5f206bf687cc9f528df84673d8508d88
Nick Tse <nicholashtse@gmail.com>
2022-09-25 03:25:50 -0700

    add swagger documentation

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using AnimalShelter.Models;
 
 namespace AnimalShelter
@@ -24,6 +25,10 @@ namespace AnimalShelter
             services.AddDbContext<AnimalShelterContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Animal Shelter Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,12 +38,13 @@ namespace AnimalShelter
             {
                 app.UseDeveloperExceptionPage();
             }
-
             // app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Animal Shelter Api v1"));
 
             app.UseEndpoints(endpoints =>
             {
