2097728505fa9deacda1b94af487b432c358239c
Nick Tse <nicholashtse@gmail.com>
2022-09-24 17:57:11 -0700

    create seeding migration and update database

---
+++ AnimalShelter/Migrations/20220925005638_SeedData.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, "Matilda", "Dog" },
+                    { 2, 10, "Rexie", "Cat" },
+                    { 3, 2, "Matilda", "Rabbit" },
+                    { 4, 4, "Pip", "Bird" },
+                    { 5, 22, "Bartholomew", "Guinea Pig" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
